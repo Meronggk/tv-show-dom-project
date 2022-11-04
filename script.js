a07bc5a8afcafb6e8d1360e5c399b446f3030b40
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-11-04 20:12:35 +0000

    added nav bar

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
+const rootElem = document.getElementById('root');
+
+
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -7,6 +13,8 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const cardElem = document.getElementById('card');
 }
 
 window.onload = setup;
+
